e89da8a5c42c539a1f04b570834e8dbe96ec9f5b
INPS\Connor.Nicol <Connor. Nicol@INPS.co.uk>
2018-08-24 09:59:15 +0100

    added the project

---
+++ UWPDummyProject.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace UWPDummyProject.Core.ViewModels
+{
+    using MvvmCross.Core.ViewModels;
+
+    /// <summary>
+    /// the Viewmodel for the main view
+    /// </summary>
+    /// <seealso cref="MvvmCross.Core.ViewModels.MvxViewModel" />
+    public class MainViewModel : MvxViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ UWPDummyProject.UWP/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace UWPDummyProject.UWP.Views
+{
+    using MvvmCross.WindowsUWP.Views;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainView : MvxWindowsPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainView"/> class.
+        /// </summary>
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

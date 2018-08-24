d0f0049000308e43bc3341e3f59328f7ad2a4187
ConnorNicolINPS <connor.nicol@inps.co.uk>
2018-08-24 14:30:19 +0100

    added useful bits of code for creating the UI of the views

---
+++ UWPDummyProject.UWP/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace UWPDummyProject.UWP.Converters
+{
+    using System;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Data;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Windows.UI.Xaml.Data.IValueConverter" />
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var invert = parameter is string && ((string)parameter).ToLowerInvariant() == "invert";
+            var val = (bool)value;
+
+            Visibility visibility;
+            if (val)
+            {
+                visibility = invert ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                visibility = invert ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
